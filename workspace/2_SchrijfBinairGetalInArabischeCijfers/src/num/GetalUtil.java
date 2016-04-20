23dee9321cfd411b1aea75e76c422d2829de5930
ilsed <ilse.dierickx@cegeka.be>
2016-04-20 08:12:40 +0200

    Create GetalUtil.java
    Oefening stond verstopt in workspaces directory

---
+++ workspace/2_SchrijfBinairGetalInArabischeCijfers/src/num/GetalUtil.java
@@ -0,0 +1,14 @@
+package num;
+
+public class GetalUtil {
+
+	/**
+	 * Converteert het gegeven binair cijfer naar een arabisch cijfer
+	 * @param binairCijfer het te converteren binair cijfer
+	 * @return het geconverteerde arabische cijfer
+	 */
+	public static int schrijfInArabischeCijfers(String binairCijfer) {
+		throw new UnsupportedOperationException("implement me");
+	}
+
+}
