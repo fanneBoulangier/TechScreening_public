e5cde7e399aa0feba5ed8fbeeba505154f5a7c19
ilsed <ilse.dierickx@cegeka.be>
2016-08-09 08:58:20 +0200

    Update HalveerInLijstTest.java
    Update deprecated Assert

--- workspace/2_HalveerInLijst/test/lists/HalveerInLijstTest.java
+++ workspace/2_HalveerInLijst/test/lists/HalveerInLijstTest.java
@@ -1,7 +1,7 @@
 package lists;
 
 import static java.util.Arrays.asList;
-import static junit.framework.Assert.assertEquals;
+import static org.junit.Assert.assertEquals;
 
 import org.junit.Test;
 
