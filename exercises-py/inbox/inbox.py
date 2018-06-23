027c418055d00973f336945e7f32d8be1e790996
Steven Op de beeck <steven@opdebeeck.org>
2018-06-23 20:41:30 +0200

    steveodb: leaving arguments to be filled in

--- exercises-py/inbox/inbox.py
+++ exercises-py/inbox/inbox.py
@@ -4,4 +4,4 @@ from inbox.message_sender import MessageSender
 class Inbox:
 
     def receive_message(self, sender: str, message: str) -> None:
-        MessageSender.send_message(sender, message)
+        MessageSender.send_message('', '')
